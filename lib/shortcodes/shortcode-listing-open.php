a5bdcccf185e88c130859dca3de7ac9d866e24da
taher <tah3r.a@gmail.com>
2016-02-06 18:09:59 +0330

    Modify listing open shortcode to supporting override template feature.

--- lib/shortcodes/shortcode-listing-open.php
+++ lib/shortcodes/shortcode-listing-open.php
@@ -13,136 +13,109 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 // Only load on front
-if( is_admin() ) {
+if ( is_admin() ) {
 	return;
 }
 /**
- * This shortcode allows for you to specify the property type(s) using 
- * [listing_open post_type="property,rental"] option. You can also 
+ * This shortcode allows for you to specify the property type(s) using
+ * [listing_open post_type="property,rental"] option. You can also
  * limit the number of entries that display. using  [epl-property-open limit="5"]
  */
 function epl_shortcode_property_open_callback( $atts ) {
 	$property_types = epl_get_active_post_types();
-	if(!empty($property_types)) {
-		 $property_types = array_keys($property_types);
+	if ( ! empty( $property_types ) ) {
+		$property_types = array_keys( $property_types );
 	}
-	
-	extract( shortcode_atts( array(
-		'post_type' 		=>	$property_types, //Post Type
-		'limit'			=>	'-1', // Number of maximum posts to show
-		'template'		=>	false, // Template. slim, table
-		'location'		=>	'', // Location slug. Should be a name like sorrento
-		'tools_top'		=>	'off', // Tools before the loop like Sorter and Grid on or off
-		'tools_bottom'		=>	'off', // Tools after the loop like pagination on or off
-		'sortby'		=>	'', // Options: price, date : Default date
-		'sort_order'		=>	'DESC'
 
-	), $atts ) );
-	
-	if(is_string($post_type) && $post_type == 'rental') {
+	$attributes = shortcode_atts( array(
+		'title'        => '',
+		'post_type'    => $property_types, //Post Type
+		'limit'        => '-1', // Number of maximum posts to show
+		'template'     => false, // Template. slim, table
+		'location'     => '', // Location slug. Should be a name like sorrento
+		'tools_top'    => 'off', // Tools before the loop like Sorter and Grid on or off
+		'tools_bottom' => 'off', // Tools after the loop like pagination on or off
+		'sortby'       => '', // Options: price, date : Default date
+		'sort_order'   => 'DESC',
+		'view'         => 'default', // Template View of listing-open shortcode
+	), $atts );
+
+	if ( is_string( $attributes['post_type'] ) && $attributes['post_type'] == 'rental' ) {
 		$meta_key_price = 'property_rent';
 	} else {
 		$meta_key_price = 'property_price';
 	}
-	
-	$sort_options = array(
-		'price'			=>	$meta_key_price,
-		'date'			=>	'post_date'
-	);
-	
-	ob_start();
-	if( !is_array($post_type) ) {
-		$post_type 			= array_map('trim',explode(',',$post_type) );
+
+	if ( ! is_array( $attributes['post_type'] ) ) {
+		$attributes['post_type'] = array_map( 'trim', explode( ',', $attributes['post_type'] ) );
 	}
 
 	$args = array(
-		'post_type' 		=>	$post_type,
-		'posts_per_page'	=>	$limit,
-		'meta_key' 		=>	'property_inspection_times',
-		'meta_query' => array(
+		'post_type'      =>	$attributes['post_type'],
+		'posts_per_page' =>	intval( $attributes['limit'] ),
+		'meta_key'       =>	'property_inspection_times',
+		'meta_query'     => array(
 			array(
-				'key' => 'property_inspection_times',
-				'value' => '',
+				'key'     => 'property_inspection_times',
+				'value'   => '',
 				'compare' => '!=',
-			)
+			),
 		)
 	);
-	
-	if(!empty($location) ) {
-		if( !is_array( $location ) ) {
-			$location = explode(",", $location);
-			$location = array_map('trim', $location);
-			
+
+	if ( ! empty( $attributes['location'] ) ) {
+		if ( ! is_array( $attributes['location'] ) ) {
+			$attributes['location'] = explode( ',', $attributes['location'] );
+			$attributes['location'] = array_map( 'trim', $attributes['location'] );
+
 			$args['tax_query'][] = array(
 				'taxonomy' => 'location',
-				'field' => 'slug',
-				'terms' => $location
+				'field'    => 'slug',
+				'terms'    => $attributes['location'],
 			);
 		}
 	}
-	
-	if( $sortby != '' ) {
-	
-		if($sortby == 'price') {
-			$args['orderby']	=	'meta_value_num';
-			$args['meta_key']	=	$meta_key_price;
+
+	if ( $attributes['sortby'] != '' ) {
+		if ( $attributes['sortby'] == 'price' ) {
+			$args['orderby']  = 'meta_value_num';
+			$args['meta_key'] = $meta_key_price;
 		} else {
-			$args['orderby']	=	'post_date';
-			$args['order']		=	'DESC';
+			$args['orderby'] = 'post_date';
+			$args['order']   = 'DESC';
+		}
+		$args['order'] = $attributes['sort_order'];
+	}
 
+	if ( isset( $_GET['sortby'] ) ) {
+		$orderby = sanitize_text_field( trim( $_GET['sortby'] ) );
+		if ( $orderby == 'high' ) {
+			$args['orderby']  = 'meta_value_num';
+			$args['meta_key'] = $meta_key_price;
+			$args['order']    = 'DESC';
+		} elseif ( $orderby == 'low' ) {
+			$args['orderby']  = 'meta_value_num';
+			$args['meta_key'] = $meta_key_price;
+			$args['order']    = 'ASC';
+		} elseif ( $orderby == 'new' ) {
+			$args['orderby'] = 'post_date';
+			$args['order']   = 'DESC';
+		} elseif ( $orderby == 'old' ) {
+			$args['orderby'] = 'post_date';
+			$args['order']   = 'ASC';
 		}
-		$args['order']			=	$sort_order;
 	}
-	
-	
-	if( isset( $_GET['sortby'] ) ) {
-		$orderby = sanitize_text_field( trim($_GET['sortby']) );
-		if($orderby == 'high') {
-			$args['orderby']	=	'meta_value_num';
-			$args['meta_key']	=	$meta_key_price;
-			$args['order']		=	'DESC';
-		} elseif($orderby == 'low') {
-			$args['orderby']	=	'meta_value_num';
-			$args['meta_key']	=	$meta_key_price;
-			$args['order']		=	'ASC';
-		} elseif($orderby == 'new') {
-			$args['orderby']	=	'post_date';
-			$args['order']		=	'DESC';
-		} elseif($orderby == 'old') {
-			$args['orderby']	=	'post_date';
-			$args['order']		=	'ASC';
-		}
-		
-	}
+
 	$query_open = new WP_Query( $args );
-	if ( $query_open->have_posts() ) { ?>
-		<div class="loop epl-shortcode">
-			<div class="loop-content epl-shortcode-listing-location <?php echo epl_template_class( $template ); ?>">
-				<?php
-					if ( $tools_top == 'on' ) {
-						do_action( 'epl_property_loop_start' );
-					}
-					while ( $query_open->have_posts() ) {
-						$query_open->the_post();
-						
-						$template = str_replace('_','-',$template);
-						epl_property_blog($template);
-					}
-					if ( $tools_bottom == 'on' ) {
-						do_action( 'epl_property_loop_end' );
-					}
 
-				?>
-			</div>
-			<div class="loop-footer">
-				<?php do_action('epl_pagination',array('query'	=>	$query_open)); ?>
-			</div>
-		</div>
-		<?php
-	} else {
-		echo '<h3 class="epl-shortcode-listing-open epl-alert">'.__('Nothing currently scheduled for inspection, please check back later.', 'epl').'</h3>';
-	}
-	wp_reset_postdata();
+	ob_start();
+	epl_get_template_part(
+		'shortcodes/listing-open/' . ( ! empty( $attributes['view'] ) ? trim( $attributes['view'] ) . '.php' : 'default.php' ),
+		array(
+			'attributes' => $attributes,
+			'query_open' => $query_open,
+		)
+	);
 	return ob_get_clean();
 }
 add_shortcode( 'home_open_list', 'epl_shortcode_property_open_callback' );
