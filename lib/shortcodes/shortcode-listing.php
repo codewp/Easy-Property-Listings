2ceda55a6990900b6d23ad9441e6f2e54f9a8f9f
taher <tah3r.a@gmail.com>
2016-02-09 01:10:37 +0330

    Adding listing_open attribute to shortcode attributes. Using EPL_Property_Listing to getting listings.

--- lib/shortcodes/shortcode-listing.php
+++ lib/shortcodes/shortcode-listing.php
@@ -43,6 +43,7 @@ function epl_shortcode_listing_callback( $atts ) {
 		'tools_bottom'      => 'off', // Tools after the loop like pagination on or off
 		'sortby'            => '', // Options: price, date : Default date
 		'sort_order'        => 'DESC',
+		'listing_open'      => 0, // Open listings.
 		'query_object'      => '', // only for internal use . if provided use it instead of custom query
 		// Map properties.
 		'show_map'          => 0,
@@ -61,137 +62,6 @@ function epl_shortcode_listing_callback( $atts ) {
 		'view'              => 'default',
 	), $atts );
 
-	if ( is_string( $attributes['post_type'] ) && $attributes['post_type'] == 'rental' ) {
-		$meta_key_price = 'property_rent';
-	} else {
-		$meta_key_price = 'property_price';
-	}
-
-	if ( ! is_array( $attributes['post_type'] ) ) {
-		$attributes['post_type'] = array_map( 'trim', explode( ',',$attributes['post_type'] ) );
-	}
-	$paged = get_query_var( 'paged', 1 ) ? get_query_var( 'paged', 1 ) : 1;
-	if ( is_front_page() ) {
-		$paged = get_query_var( 'page', 1 ) ? get_query_var( 'page', 1 ) : 1;
-	}
-	$args = array(
-		'post_type'      =>	$attributes['post_type'],
-		'posts_per_page' =>	$attributes['limit'],
-		'paged'          =>	absint( $paged ),
-	);
-	// Listings of specified author.
-	if ( ! empty( $attributes['author'] ) ) {
-		if ( is_array( $attributes['author'] ) ) {
-			$attributes['author'] = implode( ',', array_map( 'absint', $attributes['author'] ) );
-		}
-		$args['author'] = trim( $attributes['author'] );
-	}
-	// Featured listings.
-	if ( $attributes['featured'] ) {
-		$args['meta_query'][] = array(
-			'key'   => 'property_featured',
-			'value' => 'yes',
-		);
-	}
-
-	// Listing location tax query based on location slug.
-	if ( ! empty( $attributes['location'] ) ) {
-		if ( ! is_array( $attributes['location'] ) ) {
-			$attributes['location'] = array_map( 'trim', explode( ',', $attributes['location'] ) );
-		}
-		$args['tax_query'][] = array(
-			'taxonomy' => 'location',
-			'field'    => 'slug',
-			'terms'    => $attributes['location'],
-		);
-	}
-
-	// Listing location tax query based on location id.
-	if ( ! empty( $attributes['location_id'] ) ) {
-		if ( ! is_array( $attributes['location_id'] ) ) {
-			$attributes['location_id'] = array_map( 'intval', explode( ',', $attributes['location_id'] ) );
-		}
-		$args['tax_query'][] = array(
-			'taxonomy' => 'location',
-			'field'    => 'id',
-			'terms'    => $attributes['location_id'],
-		);
-	}
-
-	// Listing feature tax query based on feature slug.
-	if ( ! empty( $attributes['feature'] ) ) {
-		if ( ! is_array( $attributes['feature'] ) ) {
-			$attributes['feature'] = array_map( 'trim', explode( ',', $attributes['feature'] ) );
-		}
-		$args['tax_query'][] = array(
-			'taxonomy' => 'tax_feature',
-			'field'    => 'slug',
-			'terms'    => $attributes['feature'],
-		);
-	}
-
-	// Listing feature tax query based on feature id.
-	if ( ! empty( $attributes['feature_id'] ) ) {
-		if ( ! is_array( $attributes['feature_id'] ) ) {
-			$attributes['feature_id'] = array_map( 'intval', explode( ',', $attributes['feature_id'] ) );
-		}
-		$args['tax_query'][] = array(
-			'taxonomy' => 'tax_feature',
-			'field'    => 'id',
-			'terms'    => $attributes['feature_id'],
-		);
-	}
-
-	if ( ! empty( $attributes['status'] ) ) {
-		if ( ! is_array( $attributes['status'] ) ) {
-			$attributes['status'] = array_map( 'trim', explode( ',', $attributes['status'] ) );
-		}
-		$args['meta_query'][] = array(
-			'key'     => 'property_status',
-			'value'   => $attributes['status'],
-			'compare' => 'IN',
-		);
-		add_filter( 'epl_sorting_options', 'epl_sorting_options_callback' );
-	}
-
-	if ( ! empty ( $attributes['sortby'] ) ) {
-		if ( $attributes['sortby'] == 'price' ) {
-			$args['orderby']  = 'meta_value_num';
-			$args['meta_key'] =	$meta_key_price;
-		} else {
-			$args['orderby']  = 'post_date';
-			$args['order']    = 'DESC';
-		}
-		$args['order']        = $attributes['sort_order'];
-	}
-
-	if ( isset( $_GET['sortby'] ) ) {
-		$orderby = sanitize_text_field( trim( $_GET['sortby'] ) );
-		if ( $orderby == 'high' ) {
-			$args['orderby']  = 'meta_value_num';
-			$args['meta_key'] =	$meta_key_price;
-			$args['order']    = 'DESC';
-		} else if ( $orderby == 'low' ) {
-			$args['orderby']  = 'meta_value_num';
-			$args['meta_key'] =	$meta_key_price;
-			$args['order']    = 'ASC';
-		} else if ( $orderby == 'new' ) {
-			$args['orderby']  = 'post_date';
-			$args['order']    = 'DESC';
-		} else if ( $orderby == 'old' ) {
-			$args['orderby']  = 'post_date';
-			$args['order']    = 'ASC';
-		} else if ( $orderby == 'status_desc' ) {
-			$args['orderby']  = 'meta_value';
-			$args['meta_key'] =	'property_status';
-			$args['order']    = 'DESC';
-		} else if ( $orderby == 'status_asc' ) {
-			$args['orderby']  = 'meta_value';
-			$args['meta_key'] =	'property_status';
-			$args['order']    = 'ASC';
-		}
-	}
-
 	// Map properties.
 	if ( $attributes['show_map'] ) {
 		$attributes['map_id']            = trim( $attributes['map_id'] );
@@ -203,7 +73,15 @@ function epl_shortcode_listing_callback( $atts ) {
 		$attributes['cluster_size']      = (int) $attributes['cluster_size'];
 	}
 
-	$query_open = new WP_Query( $args );
+	// Pagination.
+	$attributes['paged'] = get_query_var( 'paged', 1 ) ? get_query_var( 'paged', 1 ) : 1;
+	if ( is_front_page() ) {
+		$attributes['paged'] = get_query_var( 'page', 1 ) ? get_query_var( 'page', 1 ) : 1;
+	}
+
+	// Getting listings.
+	$epl_property_listing = new EPL_Property_Listing( $attributes );
+	$query_open           = $epl_property_listing->get_listings();
 
 	if ( is_object( $attributes['query_object'] ) ) {
 		$query_open = $attributes['query_object'];
